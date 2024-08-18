bcce20cad4c7ef3683ee2bb97a31e39e39673bb7
Ilya Novozhilov <novozhilov@qst.hk>
2024-08-18 18:00:49 +0300

    rename things, remove unneeded stuff

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-import { App, Editor, MarkdownView, Modal, Plugin } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-export default class MyPlugin extends Plugin {
+export default class MoveByWord extends Plugin {
 	async onload() {
 		this.addCommand({
 			id: 'forward-word',
@@ -21,8 +21,8 @@ export default class MyPlugin extends Plugin {
 		});
 
 		this.addCommand({
-			id: "kill-word",
-			name: "Kill word",
+			id: "delete-word-forward",
+			name: "Delete word forward",
 			editorCallback: (editor: Editor, _: MarkdownView) => {
 				const cursorBefore = editor.getCursor();
 				editor.exec("goWordRight");
@@ -32,26 +32,6 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection("");
 			},
 		});
-	}
-
-	onunload() {
-
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
